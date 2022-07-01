c344ebc0b254fc8b5fbb56e136cd25ec519be22c
bitbubbler <105576473+bitbubbler@users.noreply.github.com>
2022-07-01 10:28:18 -0500

    try to fix codegen again?

--- generate-types.js
+++ generate-types.js
@@ -2,27 +2,36 @@
 const { rm, readdir } = require('fs/promises')
 const { resolve } = require('path')
 const util = require('util')
-const execFile = util.promisify(require('child_process').execFile);
+const exec = util.promisify(require('child_process').exec)
 
 const generatedDir = resolve('generated')
 
 async function main() {
-    // delete existing generated files
-    await rm(generatedDir, { recursive: true, force: true })
+  // delete existing generated files
+  await rm(generatedDir, { recursive: true, force: true })
 
-    const npmBin = resolve('node_modules', '.bin')
-    const bin = resolve(npmBin, 'proto-loader-gen-types')
-    const protoDir = resolve('src', 'proto')
-    const dcsProtoFile = resolve(protoDir, 'dcs', 'dcs.proto')
+  const npmBin = resolve('node_modules', '.bin')
+  const bin = resolve(npmBin, 'proto-loader-gen-types')
+  const protoDir = resolve('src', 'proto')
+  const dcsProtoFile = resolve(protoDir, 'dcs', 'dcs.proto')
 
-    const args = ['-I', protoDir, '-O', generatedDir, '--oneofs', '--grpcLib=@grpc/grpc-js', dcsProtoFile]
+  const args = [
+    '-I',
+    protoDir,
+    '-O',
+    generatedDir,
+    '--oneofs',
+    '--grpcLib=@grpc/grpc-js',
+    dcsProtoFile,
+  ]
+    .map(arg => `"${arg}"`)
+    .join(' ')
 
-    await execFile(bin, args, { shell: true }) // shell:true makes this work on windows
+  await exec(`"${bin}" ${args}`)
 
-    console.log(`generated types in ${generatedDir}`)
+  console.log(`generated types in ${generatedDir}`)
 }
 
 main().catch(error => {
-    console.error('failed', error)
+  console.error('failed', error)
 })
-
